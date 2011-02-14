6e7e8406af0a3ba83ee86af7a30328ef5478d7fd
Ahmed El-Mahdawy <aa.mahdawy.10@gmail.com>
2011-02-14 18:07:47 +0200

    reverting last commit

--- TransShell/link.py
+++ TransShell/link.py
@@ -15,11 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-try:
-  import pexpect
-except ImportError:
-  print "<> Unable to import pexpect. You are allowed to continue, but TransShell is likely to fail."
-
 class Link():
   def __init__(self, conn, command, args):
     self.conn = conn
