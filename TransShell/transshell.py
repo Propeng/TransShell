17f4ecf72d203b8b4f1bb629ef6a8d9e1ec10f6a
Ahmed El-Mahdawy <aa.mahdawy.10@gmail.com>
2011-02-16 22:02:44 +0200

    implemented user auth

--- TransShell/transshell.py
+++ TransShell/transshell.py
@@ -46,15 +46,31 @@ class TransShell():
     thread.start_new_thread(shell.shell, (self.links, conn))
 
   def privmsg(self, conn, event):
-    nick = event.source().split('!')[0]
-    self.handlemsg(event.arguments()[0], nick, nick)
+    split = event.source().split('!')
+    nick = source[0]
+    self.handlemsg(conn, event.arguments()[0], nick, nick, split[1].split('@')[0])
 
   def pubmsg(self, conn, event):
     message = event.arguments()[0]
     if message.startswith(Config.chan_prefix):
-      nick = event.source().split('!')[0]
-      self.handlemsg(message[len(Config.chan_prefix):], event.target(), nick)
+      split = event.source().split('!')
+      nick = split[0]
+      self.handlemsg(conn, message[len(Config.chan_prefix):], event.target(), nick, split[1].split('@')[0])
 
-  def handlemsg(self, message, linkto, nick):
-    if linkto in self.links: #only respond if channel is linked to
-      self.links[linkto].queue.append(message)
+  def handlemsg(self, conn, message, linkto, nick, user):
+    authed = False
+    if Config.auth_type == 0:
+      authed = True
+    elif Config.auth_type == 1:
+      authed = Config.auth_user == user
+    elif Config.auth_type == 2:
+      print "<> Auth type 2 not implemented, denying auth"
+    else:
+      print "<> Auth type unknown, denying auth"
+
+    if authed:
+      if linkto in self.links: #only respond if channel is linked to
+        self.links[linkto].queue.append(message)
+    else:
+      print "<> Auth error on %s (%s@%s)" % (linkto, nick, user)
+      conn.privmsg(linkto, "<> %s: Access denied" % nick)
